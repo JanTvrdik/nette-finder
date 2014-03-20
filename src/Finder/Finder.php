f73c012618d589976f796d840a72b64349177ba9
David Grudl <david@grudl.com>
2014-03-20 13:57:38 +0100

    removed dependency on Nette\Utils\DateTime

--- src/Finder/Finder.php
+++ src/Finder/Finder.php
@@ -356,7 +356,14 @@ class Finder extends Nette\Object implements \IteratorAggregate, \Countable
 			list(, $operator, $date) = $matches;
 			$operator = $operator ? $operator : '=';
 		}
-		$date = DateTime::from($date)->format('U');
+
+		if ($date === NULL) {
+			$date = time();
+		} elseif ($date instanceof \DateTime || $date instanceof \DateTimeInterface) {
+			$date = $date->format('U');
+		} elseif (!is_numeric($date)) {
+			$date = strtotime($date);
+		}
 		return $this->filter(function (FilesystemIterator $file) use ($operator, $date) {
 			return self::compare($file->getMTime(), $operator, $date);
 		});
